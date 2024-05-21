99c6b67c45007a0d3d5b9295951c107693614fd3
aureellia <aurelliacindy21@gmail.com>
2024-05-21 00:29:37 -0800

    menambahkan php

---
+++ README.md
@@ -0,0 +1,1 @@
+# aureellia.github.io
---
+++ script.js
@@ -0,0 +1,46 @@
+function scrollTrigger(selector, options= {}){
+    let els = document.querySelectorAll(selector)
+    els = Array.from(els)
+    els.forEach(el => {
+        addObserver(el, options)
+    })
+}
+
+function addObserver(el, options){
+if(!('IntersectionObserver' in window)){
+    if(options.cb){
+        options.cb(el)
+    }else{
+        entry.target.classList.add('active')
+    }
+    return
+}  
+let observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            if(options.cb){
+                options.cb(el)
+        }else{
+            entry.target.classList.add('atas')
+        }
+        observer.unobserver(entry.target)
+        }    
+    })
+    }, options)   
+    observer.observe(el)
+}
+
+scrollTrigger('#gallery', {
+    rootMargin: '-75px',
+
+})
+
+scrollTrigger('#blog', {
+    rootMargin: '-75px',
+
+})
+
+scrollTrigger('#contact', {
+    rootMargin: '-75px',
+
+})
